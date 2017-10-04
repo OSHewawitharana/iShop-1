ff3750e207b9d1eeb764530edba315f9f8f871f9
WeerakoonOS <osw0294@gmail.com>
2017-10-04 21:07:57 +0530

    user page2

--- admin/examples/user.php
+++ admin/examples/user.php
@@ -121,88 +121,68 @@ session_start();
             <div class="content">
                 <div class="container-fluid">
                     <div class="row">
-                        <div class="col-md-12">
+                        <div class="col-md-11">
                             <div class="card">
                                 <div class="card-header" data-background-color="purple">
                                     <h4 class="title">Edit Profile</h4>
-                                    <p class="category">Complete your profile</p>
                                 </div>
                                 <div class="card-content">
-                                    <form>
+                                    <form action="user.php" method="POST">
                                         <div class="row">
                                             <div class="col-md-5">
                                                 <div class="form-group label-floating">
-                                                    <label class="control-label">Company (diabled)</label>
-                                                    <input type="text" class="form-control" disabled>
-                                                </div>
-                                            </div>
-                                            <div class="col-md-3">
-                                                <div class="form-group label-floating">
                                                     <label class="control-label">Username</label>
-                                                    <input type="text" class="form-control">
+                                                    <input id="username" name="username" type="text" class="form-control">
                                                 </div>
                                             </div>
-                                            <div class="col-md-4">
+                                            <div class="col-md-5">
                                                 <div class="form-group label-floating">
                                                     <label class="control-label">Email address</label>
-                                                    <input type="email" class="form-control">
+                                                    <input id="email" name="email" type="email" class="form-control">
                                                 </div>
                                             </div>
                                         </div>
                                         <div class="row">
-                                            <div class="col-md-6">
+                                            <div class="col-md-5">
                                                 <div class="form-group label-floating">
-                                                    <label class="control-label">Fist Name</label>
-                                                    <input type="text" class="form-control">
+                                                    <label class="control-label">First Name</label>
+                                                    <input id="fname" name="fname" type="text" class="form-control">
                                                 </div>
                                             </div>
-                                            <div class="col-md-6">
+                                            <div class="col-md-5">
                                                 <div class="form-group label-floating">
                                                     <label class="control-label">Last Name</label>
-                                                    <input type="text" class="form-control">
+                                                    <input id="lname" name="lname" type="text" class="form-control">
                                                 </div>
                                             </div>
                                         </div>
                                         <div class="row">
-                                            <div class="col-md-12">
+                                            <div class="col-md-10">
                                                 <div class="form-group label-floating">
                                                     <label class="control-label">Address</label>
-                                                    <input type="text" class="form-control">
+                                                    <input id="address" name="address" type="text" class="form-control">
                                                 </div>
                                             </div>
                                         </div>
                                         <div class="row">
-                                            <div class="col-md-4">
-                                                <div class="form-group label-floating">
-                                                    <label class="control-label">City</label>
-                                                    <input type="text" class="form-control">
-                                                </div>
-                                            </div>
-                                            <div class="col-md-4">
-                                                <div class="form-group label-floating">
-                                                    <label class="control-label">Country</label>
-                                                    <input type="text" class="form-control">
-                                                </div>
-                                            </div>
-                                            <div class="col-md-4">
-                                                <div class="form-group label-floating">
-                                                    <label class="control-label">Postal Code</label>
-                                                    <input type="text" class="form-control">
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="row">
-                                            <div class="col-md-12">
+                                            <div class="col-md-10">
                                                 <div class="form-group">
                                                     <label>About Me</label>
                                                     <div class="form-group label-floating">
-                                                        <textarea class="form-control" rows="5"></textarea>
+                                                        <textarea id="aboutme" name="aboutme" class="form-control" rows="1"></textarea>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
+                                        <div class="col-md-5">
+                                                <div class="form-group label-floating">
+                                                    <label class="control-label">New Password</label>
+                                                    <input id="password" name="password" type="password" class="form-control">
+                                                </div>
+                                            </div>
                                         <button type="submit" class="btn btn-primary pull-right">Update Profile</button>
                                         <div class="clearfix"></div>
+
                                     </form>
                                 </div>
                             </div>
